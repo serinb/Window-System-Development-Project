f4f02a849da29b118b3847ff08e866c5cf56bfb6
Ulyana Lavnikevich <ulyana.lavnikevich@rwth-aachen.de>
2023-05-19 09:57:17 +0200

    hello world commit

--- HelloWorldRevised.py
+++ HelloWorldRevised.py
@@ -20,7 +20,18 @@ class HellowWorld:
 
     def start(self):
 
-        window = self.windowSystem.createWindowOnScreen(20, 20, 200, 200, "HelloWorld", COLOR_LIGHT_GREEN)
+        window = self.windowSystem.createWindowOnScreen(20, 20, 250, 300, "HelloWorld", COLOR_LIGHT_GREEN)
 
+        languageLabel = self.windowSystem.createLabelInWindow(window, 100, 40, 100, 20, "languageLabel",
+                                                              "Please select a language",  "#F6A800", COLOR_WHITE)
 
+        btn_german = self.windowSystem.createButtonInWindow(window, 100, 90, 70, 20, "btn_german", "Deutsch", COLOR_BLACK, COLOR_LIGHT_GRAY, lambda: self.german_selected())
+        # testButton = self.createButtonInWindow(gray_window, 50, 100, 120, 30, "Button on Yellow Window", "Click me",
+        #         # COLOR_BLACK, COLOR_LIGHT_GRAY, lambda: self.printSomething())
+
+    def german_selected(self):
+        # set the text attribute of languageLabel
+        # languageLabel.config(text="Guten Tag", width=100)
+        print("hey")
+        # languageLabel.text = ""
 
